4d9e0c247d73bd5a0979278f099dad5e6bf9336e
unknown <edus.silva.rosa@gmail.com>
2017-11-20 11:27:02 -0200

    adicionado classes para movimentacao de estoque

---
+++ gervasio/src/main/java/abobora/hackathon/gervasio/service/MovimentacaoSaida.java
@@ -0,0 +1,21 @@
+package abobora.hackathon.gervasio.service;
+
+import abobora.hackathon.gervasio.domain.MovimentacaoEstoque;
+
+/**
+ * @author Eduardo Silva Rosa
+ *		   edus.silva.rosa@gmail.com
+ *		   
+ *	   	   https://github.com/edusr
+ *	       + 55 (34) 9 9179-4411	
+ *
+ */
+public class MovimentacaoSaida implements MovimentacaoEstoqueService{
+
+	@Override
+	public void movimentarEstoque(MovimentacaoEstoque movimentacaoEstoque) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
