f0914d2de89f2ca18cbe331bfe4af08954aea143
eduardosr <eduardosr@peixoto.com.br>
2017-11-21 17:01:38 -0200

    adicionado leitor de qr code no app

--- Jerimum/app/src/main/java/hackaton/abobora/jerimum/MainActivity.java
+++ Jerimum/app/src/main/java/hackaton/abobora/jerimum/MainActivity.java
@@ -1,16 +1,38 @@
 package hackaton.abobora.jerimum;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private Button btnReadQr;
+    static final String ACTION_SCAN = "com.google.zxing.client.android.SCAN";
+    private TextView formatTxt, contentTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        formatTxt = (TextView)findViewById(R.id.scan_format);
+        contentTxt = (TextView)findViewById(R.id.scan_content);
+
+        btnReadQr = (Button) findViewById(R.id.btn_qr_code);
+
+        btnReadQr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scanQR();
+            }
+        });
     }
 
     @Override
@@ -33,5 +55,31 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void scanQR() {
+        try {
+            //start the scanning activity from the com.google.zxing.client.android.SCAN intent
+            Intent intent = new Intent(ACTION_SCAN);
+            intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+            startActivityForResult(intent, 0);
+        } catch (ActivityNotFoundException anfe) {
+            //on catch, show the download dialog
+           // showDialog(MainActivity.this, "No Scanner Found", "Download a scanner code activity?", "Yes", "No").show();
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                //get the extras that are returned from the intent
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                formatTxt.setText("FORMAT: " + format);
+                contentTxt.setText("CONTENT: " + contents);
+                Toast toast = Toast.makeText(this, "Leitura:" + contents + " Formato:" + format, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
     }
 }
